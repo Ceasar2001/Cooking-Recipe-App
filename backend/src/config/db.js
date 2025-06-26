759ae4a17af6870510ed8730f37549387a1e2d5c
Ceasar2001 <ecleoceasar1@gmail.com>
2025-06-25 20:49:17 -0700

    database setup

---
+++ backend/src/config/db.js
@@ -0,0 +1,8 @@
+import { drizzle } from "drizzle-orm/postgres-js";
+import { neon } from "@neondatabase/serverless";
+import { ENV } from "./env.js";
+import * as schema from "../db/schema.js";
+
+const sql = neon(ENV.DB_URL);
+
+export const db = drizzle(sql, { schema });
